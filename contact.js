74b48d3fb726866c35dfbcee2bc57f0e59f23c4c
Naima-Maitla <f250590@cfd.nu.edu.pk>
2025-11-30 16:21:22 +0500

    contact.html

---
+++ contact.js
@@ -0,0 +1,10 @@
+let phone = document.getElementById("phone");
+let landline = document.getElementById("landline");
+
+phone.onclick = function () {
+    alert("Phone number clicked successfully");
+};
+
+landline.onclick = function () {
+    alert("Landline number clicked successfully");
+};
